0fd6813cddf05bea0963f51f3798469612f20a84
nfebrian13 <nanaz.febrian@gmail.com>
2020-07-19 20:05:20 +0700

    commit to branch x

---
+++ git-github-demo/src/main/java/com/github/demo/Test2.java
@@ -0,0 +1,13 @@
+package com.github.demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		boolean hungry = true;
+
+		if (hungry)
+			System.out.println("eat something!");
+		else
+			System.out.println("do work");
+	}
+}
